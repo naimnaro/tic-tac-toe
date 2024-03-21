13f7a68c6a2908926704f37eb0675939199eace4
naimnaro <xez8jf@gmail.com>
2024-03-21 19:42:46 +0900

    asd
    asd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Board from './Board';
+import './App.css';
+function App() {
+  return (
+
+    <div className='App'>
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Board
